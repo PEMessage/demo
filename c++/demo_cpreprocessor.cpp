093ce4371b806377b296352c2d480cfe6362be38
PEMessage <1165739182@qq.com>
2025-01-07 09:04:22 +0800

    demo_cpreprocessor: init

---
+++ c++/demo_cpreprocessor.cpp
@@ -0,0 +1,41 @@
+#define single_sharp(x) 123 #x 123
+#define double_sharp(x) 123 ##x 123
+single_sharp(456);  // 123 "456" 123;  // trun into string
+double_sharp(456); // 123456 123;   // cat to previous token
+double_sharp(456,789); // oversize -> error: macro "double_sharp" passed 2 arguments, but takes just 1
+
+
+#define cat(x,y) x ## y
+// The 'real' cat 2 token
+cat(apple,banana); // applebanana
+cat(apple,xx banana); // applexx banana
+cat(apple, banana);  //applebanana
+
+#define va(...) 123 __VA_ARGS__ 123
+va(456,789); //  123 456,789 123;
+va(456, 789); // 123 456, 789 123; // extra space
+
+
+// https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html
+#define LOG(m, fmt, ...) (void)LOG_DEMO(LOG_ERROR, LABAL[m], fmt, __VA_ARGS__)
+LOG(1, "%s", example_string)
+LOG(1, "%s")
+// result: __VA_ARGS__ exists
+// (void)LOG_DEMO(LOG_ERROR, LABAL[1], "%s", example_string,2,3,4)
+// result: __VA_ARGS__ not exists
+// (void)LOG_DEMO(LOG_ERROR, LABAL[1], "%s", ) //  FAIL !!!
+#define LOG_double_sharp(m, fmt, ...) (void)LOG_DEMO(LOG_ERROR, LABAL[m], fmt, ##__VA_ARGS__)
+LOG_double_sharp(1, "%s", example_string)
+LOG_double_sharp(1, "%s")
+// result: __VA_ARGS__ exists
+// (void)LOG_DEMO(LOG_ERROR, LABAL[1], "%s", example_string,2,3,4)
+// result: __VA_ARGS__ not exists (will eat ',' before __VA_ARGS__)
+// (void)LOG_DEMO(LOG_ERROR, LABAL[1], "%s") // SUCCESS !! 
+
+
+#define REGISTER_SYSTEM_ABILITY_BY_ID(abilityClassName, systemAbilityId, runOnCreate) \
+    const bool abilityClassName##_##RegisterResult = \
+    SystemAbility::MakeAndRegisterAbility(new abilityClassName(systemAbilityId, runOnCreate));
+
+REGISTER_SYSTEM_ABILITY_BY_ID(TestService, TESTSERVICE_SA_ID, true)
+// const bool TestService_RegisterResult = SystemAbility::MakeAndRegisterAbility(new TestService(TESTSERVICE_SA_ID, true));
