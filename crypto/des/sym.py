5b269bf02751205af8bc30246ca2d317cea1afde
PEMessage <1165739182@qq.com>
2025-01-15 10:19:13 +0800

    des: update diagram

--- crypto/des/sym.py
+++ crypto/des/sym.py
@@ -5,10 +5,13 @@ x, y = sp.symbols('x y')
 
 # 定义 F 函数，这里假设 F 是一个简单的示例函数
 F =  sp.Function('F')
+dF =  sp.Function('dF')
+# def F(a, *args):
+#     return a
 
 # 初始化 Left 和 Right 的字典
-Left = {0: x , 1: x + F(y) }
-Right = {0: y, 1: y}
+Left = {1: x }
+Right = {1: y }
 
 
 # 计算前几个 N 的 Left 和 Right 值
@@ -16,14 +19,17 @@ N = 5  # 你可以选择其他的 N，来计算更多的项
 for n in range(2, N + 1):
 
     # 使用递推关系更新 Left 和 Right
-    Left[n] = Right[n - 1] + F(Left[n - 1])
-    Right[n] = Left[n - 1] # 示例的右边递推定义，这里简单设定为 Left[n-1]
+    # See: https://ctf-wiki.org/crypto/blockcipher/des/
+    # Round1, Diagram中每一层上方，左右的值
 
-left_expr = 'Left[n] = Right[n - 1] + F(Left[n - 1])'
-right_expr = 'Right[n] = Left[n - 1]'
+    Left[n] = Right[n-1]
+    Right[n] = Left[n-1] + F(Right[n-1])
+
+left_expr = 'Left[n] = Right[n-1]'
+right_expr = 'Right[n] = Left[n-1] + F(Right[n-1])'
 print(f'{left_expr:<100} {right_expr}')
 # 输出 Left 和 Right 的结果
-for n in range(0, N + 1):
+for n in range(1, N + 1):
     left = f'Left({n}) = {Left[n]}'
     right = f'Right({n}) = {Right[n]}'
     print(f'{left:<100} {right}')
