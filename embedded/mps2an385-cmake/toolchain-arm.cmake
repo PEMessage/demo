16c5c249f3737870bd07e3743507c56b3e39ca50
PEMessage <1165739182@qq.com>
2025-03-30 21:44:10 +0800

    embedded/mps2an385-cmake: fix clangd error(TODO: remove hardencoded)

--- embedded/mps2an385-cmake/toolchain-arm.cmake
+++ embedded/mps2an385-cmake/toolchain-arm.cmake
@@ -11,8 +11,11 @@ set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 set(CMAKE_OBJDUMP arm-none-eabi-objdump)
 set(CMAKE_SIZE arm-none-eabi-size)
 
+# Workaround to make clangd happy
+set(GCC_INCLUDE_FLAGS "-isystem /usr/lib/arm-none-eabi/include/")
+
 # Compiler flags
-set(CMAKE_C_FLAGS "-mcpu=cortex-m3 -mthumb -Wall -Os -g -g3" CACHE STRING "C Compiler Flags")
+set(CMAKE_C_FLAGS "-mcpu=cortex-m3 -mthumb -Wall -Os -g -g3 ${GCC_INCLUDE_FLAGS}" CACHE STRING "C Compiler Flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C++ Compiler Flags")
 set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "ASM Compiler Flags")
 set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -lnosys" CACHE STRING "Linker Flags")
