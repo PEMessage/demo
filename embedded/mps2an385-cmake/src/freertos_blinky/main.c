4a604723e23b469ec5dff9cbfa03317b0f4090f5
PEMessage <1165739182@qq.com>
2025-03-31 23:22:20 +0800

    embedded/mps2an385-cmake: add freertos_blinky, but fail to build

---
+++ embedded/mps2an385-cmake/src/freertos_blinky/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "CMSDK_CM3.h" // for SystemCoreClock
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+void NVIC_Init() {
+    size_t i = 0 ;
+    for ( i = 0; i < 32; i++) {
+        // NVIC_DisableIRQ(i);
+        NVIC_SetPriority((IRQn_Type)i, configMAX_SYSCALL_INTERRUPT_PRIORITY + 1);
+    }
+}
+
+void setup() {
+    extern int stdout_init (void);
+    stdout_init();
+    NVIC_Init();
+}
+
+void TaskTest() {
+    while(1) {
+        printf("Hi\n");
+        vTaskDelay( SystemCoreClock / 30 ) ;
+    }
+
+}
+
+
+int main() {
+    setup();
+    printf("Init complete\n");
+    xTaskCreate( TaskTest,
+            "Task1",
+            configMINIMAL_STACK_SIZE + 3*1024,
+            NULL,
+            (tskIDLE_PRIORITY + 1),
+            NULL );
+    vTaskStartScheduler();
+    while(1);
+}
