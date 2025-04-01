95a0a70c095ebf0d418c2d1a166b38c907a47708
PEMessage <1165739182@qq.com>
2025-04-01 23:20:11 +0800

    crypto/libopenssl/print_cert: compare_pubkey fallback to Publickey if fail

--- crypto/libopenssl/print_cert/src/compare_pubkey/main.c
+++ crypto/libopenssl/print_cert/src/compare_pubkey/main.c
@@ -1,84 +1,107 @@
-
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
+#include <openssl/err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
-#include <openssl/x509.h>
-#include <openssl/evp.h>
-#include <openssl/bio.h>
+#include <string.h>
 
 void handle_openssl_error() {
     ERR_print_errors_fp(stderr);
     exit(EXIT_FAILURE);
 }
 
-int compare_public_keys(const char *file1, const char *file2) {
-    FILE *fp1 = NULL, *fp2 = NULL;
-    EVP_PKEY *key1 = NULL, *key2 = NULL;
-    int result = 0;
+EVP_PKEY *load_pubkey(const char *filename) {
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) {
+        perror("fopen failed");
+        return NULL;
+    }
 
-    // Initialize OpenSSL
+    // Read entire file
+    fseek(fp, 0, SEEK_END);
+    long file_len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    
+    unsigned char *file_buf = malloc(file_len);
+    if (!file_buf) {
+        perror("malloc failed");
+        fclose(fp);
+        return NULL;
+    }
+    
+    if (fread(file_buf, 1, file_len, fp) != (size_t)file_len) {
+        perror("fread failed");
+        free(file_buf);
+        fclose(fp);
+        return NULL;
+    }
+    fclose(fp);
+
+    const unsigned char *p = file_buf;
+    EVP_PKEY *pkey = NULL;
+
+    // First try d2i_PUBKEY (standard format)
+    pkey = d2i_PUBKEY(NULL, &p, file_len);
+    if (pkey != NULL) {
+        free(file_buf);
+        return pkey;
+    }
+
+    // If that failed, reset and try PKCS#1 format for RSA
+    ERR_clear_error();
+    p = file_buf;
+    pkey = d2i_PublicKey(EVP_PKEY_RSA, NULL, &p, file_len);
+    if (pkey != NULL) {
+        free(file_buf);
+        return pkey;
+    }
+
+    // If that failed, reset and try PKCS#1 format for EC
+    ERR_clear_error();
+    p = file_buf;
+    pkey = d2i_PublicKey(EVP_PKEY_EC, NULL, &p, file_len);
+
+    free(file_buf);
+    return pkey;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <pubkey1> <pubkey2>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
 
-    // Read first public key
-    fp1 = fopen(file1, "rb");
-    if (!fp1) {
-        fprintf(stderr, "Error opening file %s\n", file1);
-        goto cleanup;
-    }
-    // Read second public key
-    fp2 = fopen(file2, "rb");
-    if (!fp2) {
-        fprintf(stderr, "Error opening file %s\n", file2);
-        goto cleanup;
-    }
-
-    // Read DER file directly as public key
-    key1 = d2i_PUBKEY_fp(fp1, NULL);
-    if (!key1) {
-        fprintf(stderr, "Error reading DER public key from %s\n", file1);
+    EVP_PKEY *pkey1 = load_pubkey(argv[1]);
+    if (!pkey1) {
+        fprintf(stderr, "Failed to load first public key\n");
         handle_openssl_error();
     }
 
-    key2 = d2i_PUBKEY_fp(fp2, NULL);
-    if (!key2) {
-        fprintf(stderr, "Error reading DER public key from %s\n", file2);
+    EVP_PKEY *pkey2 = load_pubkey(argv[2]);
+    if (!pkey2) {
+        fprintf(stderr, "Failed to load second public key\n");
+        EVP_PKEY_free(pkey1);
         handle_openssl_error();
     }
 
-    // Compare the keys
-    result = EVP_PKEY_cmp(key1, key2);
-    if (result == 1) {
-        printf("Public keys are identical\n");
-    } else if (result == 0) {
+    int cmp_result = EVP_PKEY_cmp(pkey1, pkey2);
+    EVP_PKEY_free(pkey1);
+    EVP_PKEY_free(pkey2);
+
+    if (cmp_result == 1) {
+        printf("Public keys are equal\n");
+        return EXIT_SUCCESS;
+    } else if (cmp_result == 0) {
         printf("Public keys are different\n");
+        return EXIT_SUCCESS;
     } else {
         fprintf(stderr, "Error comparing public keys\n");
         handle_openssl_error();
     }
 
-cleanup:
-    if (key1) EVP_PKEY_free(key1);
-    if (key2) EVP_PKEY_free(key2);
-    if (fp1) fclose(fp1);
-    if (fp2) fclose(fp2);
-
-    return result;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <public_key1.der> <public_key2.der>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    int comparison_result = compare_public_keys(argv[1], argv[2]);
-
-    // Clean up OpenSSL
-    // ZJW_C EVP_cleanup();
-    // ZJW_C CRYPTO_cleanup_all_ex_data();
-    // ZJW_C ERR_free_strings();
-
-    return (comparison_result == 1) ? EXIT_SUCCESS : EXIT_FAILURE;
+    return EXIT_SUCCESS;
 }
