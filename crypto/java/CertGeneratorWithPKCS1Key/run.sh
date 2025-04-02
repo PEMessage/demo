8734c26350d36b7d594e64a1b921383a148f2c7c
PEMessage <1165739182@qq.com>
2025-04-03 01:29:59 +0800

    crypto/java/CertGeneratorWithPKCS1Key: fix missing class

--- crypto/java/CertGeneratorWithPKCS1Key/run.sh
+++ crypto/java/CertGeneratorWithPKCS1Key/run.sh
@@ -5,7 +5,11 @@ fi
 if [ ! -f bcpkix-jdk15on-1.70.jar ] ; then
     wget https://repo1.maven.org/maven2/org/bouncycastle/bcpkix-jdk15on/1.70/bcpkix-jdk15on-1.70.jar
 fi
+if [ ! -f bcutil-jdk15on-1.70.jar ] ; then
+    wget https://repo1.maven.org/maven2/org/bouncycastle/bcutil-jdk15on/1.70/bcutil-jdk15on-1.70.jar
+fi
 
 
-javac -cp bcprov-jdk15on-1.70.jar:bcpkix-jdk15on-1.70.jar CertGeneratorWithPKCS1Key.java -d target/
-java  -cp target/:bcprov-jdk15on-1.70.jar:bcpkix-jdk15on-1.70.jar CertGeneratorWithPKCS1Key $1
+javac -cp bcprov-jdk15on-1.70.jar:bcpkix-jdk15on-1.70.jar:bcutil-jdk15on-1.70.jar CertGeneratorWithPKCS1Key.java -d target/
+echo java  -cp target/:bcprov-jdk15on-1.70.jar:bcpkix-jdk15on-1.70.jar CertGeneratorWithPKCS1Key $1
+java  -cp target/:bcprov-jdk15on-1.70.jar:bcpkix-jdk15on-1.70.jar:bcutil-jdk15on-1.70.jar CertGeneratorWithPKCS1Key $1
