92f6b2f70a195c40a01f19507a80d39cfc5e4aa8
PEMessage <1165739182@qq.com>
2025-04-07 23:27:37 +0800

    embedded/mps2an385-cmake: move vAssertCalled to FreeRTOS-Hook, and add vApplicationStackOverflowHook

---
+++ embedded/mps2an385-cmake/lib/FreeRTOS-Hook/hook.c
@@ -0,0 +1,44 @@
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vAssertCalled( const char * pcFileName,
+                    uint32_t ulLine )
+{
+    volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
+
+    /* Called if an assertion passed to configASSERT() fails.  See
+     * http://www.freertos.org/a00110.html#configASSERT for more information. */
+
+    printf( "ASSERT! Line %d, file %s\r\n", ( int ) ulLine, pcFileName );
+
+    taskENTER_CRITICAL();
+    {
+        /* You can step out of this function to debug the assertion by using
+         * the debugger to set ulSetToNonZeroInDebuggerToContinue to a non-zero
+         * value. */
+        while( ulSetToNonZeroInDebuggerToContinue == 0 )
+        {
+            __asm volatile ( "NOP" );
+            __asm volatile ( "NOP" );
+        }
+    }
+    taskEXIT_CRITICAL();
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask,
+                                    char * pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+     * configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+     * function is called if a stack overflow is detected. */
+    printf( "\n\n===Stack overflow in %s===\n", pcTaskName );
+    // DEVTODO: portDISABLE_INTERRUPTS();
+
+    for( ; ; )
+    {
+    }
+}
--- embedded/mps2an385-cmake/src/freertos_blinky/main.c
+++ embedded/mps2an385-cmake/src/freertos_blinky/main.c
@@ -3,30 +3,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vAssertCalled( const char * pcFileName,
-                    uint32_t ulLine )
-{
-    volatile uint32_t ulSetToNonZeroInDebuggerToContinue = 0;
-
-    /* Called if an assertion passed to configASSERT() fails.  See
-     * http://www.freertos.org/a00110.html#configASSERT for more information. */
-
-    printf( "ASSERT! Line %d, file %s\r\n", ( int ) ulLine, pcFileName );
-
-    taskENTER_CRITICAL();
-    {
-        /* You can step out of this function to debug the assertion by using
-         * the debugger to set ulSetToNonZeroInDebuggerToContinue to a non-zero
-         * value. */
-        while( ulSetToNonZeroInDebuggerToContinue == 0 )
-        {
-            __asm volatile ( "NOP" );
-            __asm volatile ( "NOP" );
-        }
-    }
-    taskEXIT_CRITICAL();
-}
-
 void NVIC_Init() {
     size_t i = 0 ;
     for ( i = 0; i < 32; i++) {
@@ -44,7 +20,7 @@ void setup() {
 void TaskPing() {
     while(1) {
         printf("Ping!\n");
-        vTaskDelay(1000);
+        vTaskDelay(300);
     }
 
 }
@@ -52,11 +28,10 @@ void TaskPing() {
 void TaskPong() {
     while(1) {
         printf("Pong!\n");
-        vTaskDelay(1700);
+        vTaskDelay(40);
     }
 
 }
-
 
 int main() {
     setup();
