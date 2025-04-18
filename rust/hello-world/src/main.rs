c40e13f963c83df12b0ce485fecd13a6912eaf11
PEMessage <1165739182@qq.com>
2025-04-19 03:16:59 +0800

    rust: learn simple trait

--- rust/hello-world/src/main.rs
+++ rust/hello-world/src/main.rs
@@ -2,10 +2,24 @@
 
 use std::io;
 
+#[derive(Debug, Default)]
+struct Point {
+    x: u32,
+    y: u32,
+}
 
 
 fn main() {
+    // Part 1, hello world
+    println!("Hello, world!");
 
+
+    // Part 2, basic input
+    print!("Please input something: ");
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout"); // we have to flash
+                                                                          // before input
+                                                                          // Rust 的标准输出（stdout）默认是 行缓冲 的（在终端里）
+                                                                          // 即遇到换行符（\n）才会真正输出
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
@@ -14,5 +28,10 @@ fn main() {
 
     println!("You entered: {}", input);
 
-    println!("Hello, world!");
+    // Part 3, learn basic trait Debug / Default
+    let point1  = Point::default();
+    let point2 = Point { x: 1,  ..Point::default() };
+
+    dbg!(point1);
+    dbg!(point2);
 }
