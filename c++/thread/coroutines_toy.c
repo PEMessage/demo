9f7e943f26bd69478e800ddedba6bfa59c20a20b
PEMessage <1165739182@qq.com>
2025-04-24 02:03:43 +0800

    c++/thread: add coroutines_toy.c

---
+++ c++/thread/coroutines_toy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Thanks to https://www.chiark.greenend.org.uk/~sgtatham/coroutines.html
+// Core hacks is Duff's device
+// 在实际应用中，这种玩具版的协程实现不太可能有用，因为它依赖于 static 变量，因此无法做到重新进入或多线程
+#define crBegin static int state=0; switch(state) { case 0:
+#define crReturn(i,x) do { state=i; return x; case i:; } while (0)
+#define crFinish }
+
+int function(void) {
+    static int i;
+    crBegin;
+    for (i = 0; i < 10; i++)
+        crReturn(1, i);
+    crFinish;
+}
+
+int main(int argc, char *argv[])
+{
+    int i; 
+    for ( i = 0 ; i < 10 ; i ++ ) {
+        printf("comsumer loop : %d\n", function());
+    }
+    return 0;
+}
