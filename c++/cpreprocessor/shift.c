3f73a771dbf5d826cfca9da2b0105f078321c32c
PEMessage <1165739182@qq.com>
2025-05-02 04:06:19 +0800

    c++/cpreprocessor: add demo subcmd parse

--- c++/cpreprocessor/shift.c
+++ c++/cpreprocessor/shift.c
@@ -30,12 +30,33 @@ int shift_demo_v2(int argc, char **argv) {
     return 0;
 }
 
+#include <string.h>
+int parse(int argc, char **argv) {
+    const char *program_name = shift(argc, argv);
+
+    if (argc > 0) {
+        const char *command_name = shift(argc, argv);
+        if (strcmp(command_name, "version") == 0) {
+            printf("%s Version is 1.0.0\n", program_name);
+            return 0;
+        } else {
+            fprintf(stderr, "Usage: %s [version]\n", program_name);
+            fprintf(stderr, "ERROR: unknown command %s\n", command_name);
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
-    printf("shift v1\n");
+    printf("[Info]: shift v1\n");
     shift_demo(argc, argv);
-    printf("shift v2 (not recommand)\n");
+    printf("[Info]: shift v2 (not recommand)\n");
     shift_demo_v2(argc, argv);
+    printf("[Info]: demo cmdline parse \n");
+    parse(argc, argv);
 
     return 0;
 }
