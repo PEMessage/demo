4aa6545286de02eb27b0a6c6bff098297e4c2788
PEMessage <1165739182@qq.com>
2025-05-08 21:30:26 +0800

    python3: gen-boilerplate clean code, and prepare for cpp-swap

--- python3/gen-boilerplate.py
+++ python3/gen-boilerplate.py
@@ -7,15 +7,20 @@ class Template(ABC):
     @abstractmethod
     def add_arguments(cls, subparser):
         pass
-    
+
     @classmethod
     @abstractmethod
     def generate(cls, args):
         pass
 
+def UpCase(in: str):
+    return field[0].upper() + field[1:]
+def lowCase(in: str):
+    return field[0].lower() + field[1:]
+
 class JavaMapClassTemplate(Template):
     """Generates a Java class that uses a Map to store fields"""
-    
+
     @classmethod
     def add_arguments(cls, subparser):
         subparser.add_argument('--class', dest='class_name', default='A',
@@ -23,7 +28,11 @@ class JavaMapClassTemplate(Template):
         subparser.add_argument('--fields', nargs='+', required=True,
                              type=cls.parse_field,
                              help='Fields in format "FieldName:Type" (e.g., "Age:int")')
-    
+        subparser.add_argument('--gentype',
+                               default='java',
+                               choices=['java', 'cpp-swap'],
+                               help='Type of generator')
+
     @staticmethod
     def parse_field(field_arg):
         try:
@@ -31,9 +40,22 @@ class JavaMapClassTemplate(Template):
             return (name, type_)
         except ValueError:
             raise argparse.ArgumentTypeError(f"Field '{field_arg}' should be in format 'name:type'")
-    
+
     @classmethod
     def generate(cls, args):
+        if args.gentype == "java":
+            return cls.java_generate(args)
+        elif args.gentype == "cpp-swap":
+            pass
+
+    @classmethod
+    def cpp_swap_generate(cls, args):
+        class_name = args.class_name
+        fields = args.fields
+        pass
+
+    @classmethod
+    def java_generate(cls, args):
         print(f"public class {args.class_name} {{")
         print("    private Map<String, Object> data;\n")
         print(f"    public {args.class_name}() {{")
@@ -60,7 +82,7 @@ class JavaMapClassTemplate(Template):
 
 class JavaPojoTemplate(Template):
     """Generates a standard Java POJO with private fields and getters/setters"""
-    
+
     @classmethod
     def add_arguments(cls, subparser):
         subparser.add_argument('--class', dest='class_name', default='A',
@@ -68,7 +90,7 @@ class JavaPojoTemplate(Template):
         subparser.add_argument('--fields', nargs='+', required=True,
                              type=cls.parse_field,
                              help='Fields in format "FieldName:Type" (e.g., "Age:int")')
-    
+
     @staticmethod
     def parse_field(field_arg):
         try:
@@ -76,16 +98,16 @@ class JavaPojoTemplate(Template):
             return (name, type_)
         except ValueError:
             raise argparse.ArgumentTypeError(f"Field '{field_arg}' should be in format 'name:type'")
-    
+
     @classmethod
     def generate(cls, args):
         print(f"public class {args.class_name} {{")
-        
+
         # Generate fields
         for field, T in args.fields:
             print(f"    private {T} {field[0].lower() + field[1:]};")
         print()
-        
+
         # Generate constructor
         params = []
         assignments = []
@@ -93,11 +115,11 @@ class JavaPojoTemplate(Template):
             param_name = field[0].lower() + field[1:]
             params.append(f"{T} {param_name}")
             assignments.append(f"        this.{param_name} = {param_name};")
-        
+
         print(f"    public {args.class_name}({', '.join(params)}) {{")
         print('\n'.join(assignments))
         print("    }\n")
-        
+
         # Generate getters and setters
         for field, T in args.fields:
             param_name = field[0].lower() + field[1:]
@@ -105,37 +127,38 @@ class JavaPojoTemplate(Template):
             print(f"    public {T} get{func_param_name}() {{")
             print(f"        return this.{param_name};")
             print("    }\n")
-            
+
             print(f"    public void set{func_param_name}({T} {param_name}) {{")
             print(f"        this.{param_name} = {param_name};")
             print("    }\n")
-        
+
         print("}")
 
 ALLTEMPLATE = {
     'java-map': JavaMapClassTemplate,
     'java-pojo': JavaPojoTemplate
 }
-    
+
 
 def main():
     parser = argparse.ArgumentParser(description='Code generator for various templates.')
-    subparsers = parser.add_subparsers(dest='template', required=True, 
-                                     help='Template to use for code generation')
-    
+    subparsers = parser.add_subparsers(dest='template', required=True,
+                                       help='Template to use for code generation')
+
     # Register all available templates
     for template_name, template_class in ALLTEMPLATE.items():
         subparser = subparsers.add_parser(template_name, help=template_class.__doc__)
         template_class.add_arguments(subparser)
         subparser.set_defaults(template_class=template_class)
-    
+
     args = parser.parse_args()
-    
+
     if not hasattr(args, 'template_class'):
         parser.print_help()
         return
-    
+
     args.template_class.generate(args)
+
 
 if __name__ == "__main__":
     main()
