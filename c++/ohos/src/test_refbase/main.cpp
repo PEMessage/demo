3ea627d733f4eec75ca7befa2c8d67862871b7ff
PEMessage <1165739182@qq.com>
2025-05-19 00:23:25 +0800

    c++/ohos: test_refbase add create_in_stack fault case demo

--- c++/ohos/src/test_refbase/main.cpp
+++ c++/ohos/src/test_refbase/main.cpp
@@ -6,14 +6,18 @@ using namespace std;
 
 struct A: RefBase {
     A() {
-        printf("Create class A: %p\n", this);
+        printf("|  Create class A: %p\n", this);
     }
     ~A() {
-        printf("~ class A: %p\n", this);
+        printf("|  ~ class A: %p\n", this);
     }
 };
 
-int main (int argc, char *argv[]) {
+
+void test_normal() {
+    printf("--------------------------------------\n");
+    printf("Func: %s\n", __func__);
+    printf("--------------------------------------\n");
     printf("=================\n");
     printf("Create a in Stack\n");
     printf("=================\n");
@@ -25,10 +29,24 @@ int main (int argc, char *argv[]) {
     printf("=================\n");
     printf("Create sptr pb\n");
     printf("=================\n");
-    sptr<A> pb = pa;
+    sptr<A> pc = pa;
 
     printf("=================\n");
     printf("return\n");
     printf("=================\n");
+}
+
+void test_create_in_stack() {
+    // if you create some refbase in stack, it should never be !!!never!!! mangered by sptr
+    A a;
+    {
+        sptr<A> pa = &a;
+        
+    } // pa delete, object 'a' do not have any strong ref now, so delete a
+} // double free HERE
+
+int main (int argc, char *argv[]) {
+    test_normal();
+    // test_create_in_stack(); // THIS will cause dounle free
     return 0;
 }
