1e1c95742f39c97023c4088e2efa85dcb46420d2
PEMessage <1165739182@qq.com>
2025-05-26 02:30:37 +0800

    c++/template: add fold_expr.cpp, some more note

--- c++/template/fold_expr.cpp
+++ c++/template/fold_expr.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+// 折叠表达式（Fold Expressions ）是 C++17 引入的一项强大特性 
+// ，专门用于在可变参数模板中简洁地处理参数包（parameter packs） 。
+// 它特别适合对一组参数进行逐个操作并合并结果 的场景。
 using namespace std;
 // 近距离接触C++折叠表达式 - Coder Arthur的文章 - 知乎
 // https://zhuanlan.zhihu.com/p/690794532
 // Using https://cppinsights.io/
+//
+// Also:
+// https://www.zhihu.com/question/813637500/answer/6540279171
 template <typename... Args>
 auto LeftFold(Args... args)
 {
