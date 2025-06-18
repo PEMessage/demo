660f05eb8ba5c49ef902b50ace4483a9929d392d
PEMessage <1165739182@qq.com>
2025-06-19 00:01:47 +0800

    c++/cmake: playground add list_append demo, fix error usage: you must not quote when join a list

--- c++/cmake/playground-script/list_append/main.cmake
+++ c++/cmake/playground-script/list_append/main.cmake
@@ -3,9 +3,16 @@ set(LINKER_OPTION "")
 # Append each linker option separately
 list(APPEND LINKER_OPTION "-Wl,--cmse-implib")
 list(APPEND LINKER_OPTION "-Wl,--sort-section=alignment")
-list(APPEND LINKER_OPTION "-Wl,--out-implib=cmse_import.o")
+list(APPEND LINKER_OPTION "-Wl,--out-implib=cmse_import.o dummy")
 
 # Join the list into a single string for use in linker flags
 string(JOIN " " LINKER_OPTION ${LINKER_OPTION})
 
 message(STATUS "Linker options: ${LINKER_OPTION}")
+
+
+set(MPS2AN505_MCU_FLAGS "")
+list(APPEND MPS2AN505_MCU_FLAGS -mcpu=cortex-m33 -mthumb -mcmse)
+list(APPEND MPS2AN505_MCU_FLAGS -DARMCM33_DSP_FP_TZ -DC_SECURE_CODE)
+string(JOIN " " MPS2AN505_MCU_FLAGS ${MPS2AN505_MCU_FLAGS})
+message(STATUS "MCUFLAGS options: ${MPS2AN505_MCU_FLAGS}")
