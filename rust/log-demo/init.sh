6409806216d2aa83821e96044725df31e6df5748
PEMessage <1165739182@qq.com>
2025-06-25 00:56:29 +0800

    rust: add log-demo

---
+++ rust/log-demo/init.sh
@@ -0,0 +1,5 @@
+cargo new log-demo
+cd log-demo
+cargo add log
+cargo add env_logger
+RUST_LOG=debug cargo run
---
+++ rust/log-demo/src/main.rs
@@ -0,0 +1,6 @@
+use log::{debug, error, info, warn};
+
+fn main() {
+    env_logger::init();
+    info!("Starting the application");
+}
