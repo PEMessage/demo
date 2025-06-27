9f8510405bb557c1a33b6538f729c668ef8c2d35
PEMessage <1165739182@qq.com>
2025-06-27 20:02:54 +0800

    c++/left_right_value: add more case, const vs none-const

--- c++/left_right_value/right_reference.cpp
+++ c++/left_right_value/right_reference.cpp
@@ -1,4 +1,4 @@
-// q-gcc: -fno-elide-constructors -- 
+// q-gcc: -fno-elide-constructors -O0 -- 
 #include <iostream>
 #include <utility>
 
@@ -7,17 +7,23 @@ using namespace std;
 class X {
 public:
     X() { std::cout << "X default ctor (" << this << ")" << std::endl; }
-    X(const X& x)  { std::cout << "X const copy ctor (" << this << ") from (" << &x << ")" << std::endl; }
-    X(const X&& x) { std::cout << "X const move ctor (" << this << ") from (" << &x << ")" << std::endl; }
-    X(X& x)  { std::cout << "X copy ctor (" << this << ") from (" << &x << ")" << std::endl; }
-    X(X&& x) { std::cout << "X move ctor (" << this << ") from (" << &x << ")" << std::endl; }
+    X(X& x)  { std::cout << "X copy ctor (" << this << ") from (" << &x << ")" << std::endl; }  // 1
+    X(X&& x) { std::cout << "X move ctor (" << this << ") from (" << &x << ")" << std::endl; }  // 2
+    X(const X& x)  { std::cout << "X const copy ctor (" << this << ") from (" << &x << ")" << std::endl; } // 3
+    X(const X&& x) { std::cout << "X const move ctor (" << this << ") from (" << &x << ")" << std::endl; } // 4
 
     ~X() { std::cout << "X dtor (" << this << ")" << std::endl; }
-    void show() { std::cout << "Show X (" << this << ")" << std::endl; }
+    void show()             { std::cout << "Show X (" << this << ")" << std::endl; }
+    void show_const() const { std::cout << "Show X (" << this << ")" << std::endl; }
 };
 
 X make_x() {
     X x1;
+    return x1;
+}
+
+const X make_x_const() {
+    const X x1;
     return x1;
 }
 
@@ -31,15 +37,33 @@ int main() {
     {
         cout << endl;
         cout << "--- X &&x = make_x()" << endl;
-        X &&x = make_x();
+        X &&x = make_x(); // if comment some constructors, it will fallback
+                          // priority is : 2 -> 4 -> 1 -> 3
+                          // move -> copy and none-const -> const
         x.show();
         cout << endl;
     }
     {
         cout << endl;
         cout << "--- X x = make_x()" << endl;
-        X x = make_x();
+        X x = make_x(); // if comment some constructors, it will fallback
+                          // priority is : 2 -> 4 -> 1 -> 3
+                          // move -> copy and none-const -> const
         x.show();
+    }
+    {
+        cout << endl;
+        cout << "--- X x = make_x_const()" << endl;
+        X x = make_x_const(); // return a const, but still using move+none-const
+        x.show();
+    }
+    {
+        cout << endl;
+        cout << "--- const X &&x = make_x_const()" << endl;
+        // X &&x = make_x_const(); // compiler error, binding reference of type ‘X&&’ to ‘const X’ discards qualifiers
+        const X &&x = make_x_const();
+        // x.show();  // compiler error
+        x.show_const();
     }
     {
         cout << endl;
@@ -60,5 +84,5 @@ int main() {
         X x;
         X x2 = move(x);
         x2.show();
-    } // after move x2/x will de deconstruct
+    } // after move x2/x will both call deconstruct
 }
