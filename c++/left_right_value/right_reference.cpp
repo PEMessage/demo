46e2b3b01f5554d4f9699b096f9099c869225381
PEMessage <1165739182@qq.com>
2025-06-27 20:02:41 +0800

    c++/left_right_value: add more case

--- c++/left_right_value/right_reference.cpp
+++ c++/left_right_value/right_reference.cpp
@@ -1,14 +1,19 @@
 // q-gcc: -fno-elide-constructors -- 
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
 class X {
 public:
-    X() { std::cout << "X ctor" << std::endl; }
-    X(const X& x) { std::cout << "X copy ctor" << std::endl; }
-    ~X() { std::cout << "X dtor" << std::endl; }
-    void show() { std::cout << "Show X" << std::endl; }
+    X() { std::cout << "X default ctor (" << this << ")" << std::endl; }
+    X(const X& x)  { std::cout << "X const copy ctor (" << this << ") from (" << &x << ")" << std::endl; }
+    X(const X&& x) { std::cout << "X const move ctor (" << this << ") from (" << &x << ")" << std::endl; }
+    X(X& x)  { std::cout << "X copy ctor (" << this << ") from (" << &x << ")" << std::endl; }
+    X(X&& x) { std::cout << "X move ctor (" << this << ") from (" << &x << ")" << std::endl; }
+
+    ~X() { std::cout << "X dtor (" << this << ")" << std::endl; }
+    void show() { std::cout << "Show X (" << this << ")" << std::endl; }
 };
 
 X make_x() {
@@ -16,17 +21,44 @@ X make_x() {
     return x1;
 }
 
+X make_x_notmp() {
+    return X();
+}
+
 
 
 int main() {
     {
-        cout << "right reference!!" << endl;
-        X &&x2 = make_x();
-        x2.show();
+        cout << endl;
+        cout << "--- X &&x = make_x()" << endl;
+        X &&x = make_x();
+        x.show();
+        cout << endl;
     }
     {
-        cout << "normal copy ctor!!" << endl;
-        X x3 = make_x();
-        x3.show();
+        cout << endl;
+        cout << "--- X x = make_x()" << endl;
+        X x = make_x();
+        x.show();
     }
+    {
+        cout << endl;
+        cout << "--- X &&x = make_x_notmp() // optimize to X x;" << endl;
+        X &&x = make_x_notmp();
+        x.show();
+        cout << endl;
+    }
+    {
+        cout << endl;
+        cout << "--- X x = make_x_notmp() // optimize to X x;" << endl;
+        X x = make_x_notmp();
+        x.show();
+    }
+    {
+        cout << endl;
+        cout << "--- X x2 = move(x)" << endl;
+        X x;
+        X x2 = move(x);
+        x2.show();
+    } // after move x2/x will de deconstruct
 }
