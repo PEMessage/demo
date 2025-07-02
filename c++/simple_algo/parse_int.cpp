a33af72a39b5340f32811778f6b0ec0d33b25586
PEMessage <1165739182@qq.com>
2025-07-03 00:05:28 +0800

    c++/simple_algo: add parse_int, add compare to istream

--- c++/simple_algo/parse_int.cpp
+++ c++/simple_algo/parse_int.cpp
@@ -1,11 +1,11 @@
 
-#include <iostream>
 #include <string_view>
-#include <string>
+#include <sstream>  // for std::istringstream
 
 // See: https://www.youtube.com/watch?v=n1sJtsjbkKo&t=793s
 // Modern C++ Error Handling - Phil Nash - CppCon 2024
 
+// similar behavior to istream
 int parse_int(std::string_view number) {
     int acc = 0;
     for (char c : number) {
@@ -20,8 +20,24 @@ int parse_int(std::string_view number) {
 
 int main() {
 
-    printf("%d\n", parse_int("42"));
-    printf("%d\n", parse_int("42x"));
-    printf("%d\n", parse_int("x42"));
+    printf("Using parse_int:\n");
+    printf("%d\n", parse_int("42"));  // 42
+    printf("%d\n", parse_int("42x")); // 42
+    printf("%d\n", parse_int("x42")); // 0
+
+    printf("\n");
+
+    printf("Using istringstream:\n");
+    auto try_parse = [](const std::string& s) {
+        std::istringstream iss(s);
+        int value;
+        iss >> value;
+        return value;
+    };
+
+    printf("%d\n", try_parse("42"));  // 42
+    printf("%d\n", try_parse("42x")); // 42
+    printf("%d\n", try_parse("x42")); // 0
+
     return 0;
 }
