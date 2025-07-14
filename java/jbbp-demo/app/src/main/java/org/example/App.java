7bf379b5d05bb09278d1f9a051ca283b22bbc2d0
PEMessage <1165739182@qq.com>
2025-07-15 01:07:57 +0800

    java/jbbp-demo: add helloworld demo

--- java/jbbp-demo/app/src/main/java/org/example/App.java
+++ java/jbbp-demo/app/src/main/java/org/example/App.java
@@ -3,6 +3,8 @@
  */
 package org.example;
 
+import org.example.binary.Demo;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
@@ -10,5 +12,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        Demo.main(new String[] {});
     }
 }
---
+++ java/jbbp-demo/app/src/main/java/org/example/binary/BytesUtils.java
@@ -0,0 +1,42 @@
+package org.example.binary;
+
+
+public class BytesUtils {
+
+    /**
+     * Converts a hexadecimal string to a byte array
+     * @param s the string to be converted, length must be even
+     * @return the converted byte array, length is half of the string length
+     */
+    public static byte[] hexToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                  + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    /**
+     * Converts binary data to a hexadecimal string
+     *
+     * @param data the binary data to be converted
+     * @return the converted hexadecimal string
+     */
+    public static String bytes2HexString(byte[] data) {
+        if (data == null) {
+            return "";
+        }
+        StringBuilder buffer = new StringBuilder();
+        for (byte b : data) {
+            String hex = Integer.toHexString(b & 0xff);
+            if (hex.length() == 1) {
+                buffer.append('0');
+            }
+            buffer.append(hex);
+        }
+        return buffer.toString().toUpperCase();
+    }
+
+}
