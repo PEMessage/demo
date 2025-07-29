9a5a1472a7aeec3ef3a299b863fa1ac2ec6e313c
PEMessage <1165739182@qq.com>
2025-07-30 00:49:43 +0800

    c++/unix/io: ioctl, using reflection to pretty print

--- c++/unix/io/ioctl/ioctl.cpp
+++ c++/unix/io/ioctl/ioctl.cpp
@@ -11,10 +11,10 @@
 #include <iostream>
 #include "visit_struct/visit_struct.hpp"
 
+VISITABLE_STRUCT(winsize, ws_row, ws_col, ws_xpixel, ws_ypixel);
+
 #define IOCTL_CMD     TIOCGWINSZ
 typedef struct winsize IOCTL_STRUCT;
-VISITABLE_STRUCT(IOCTL_STRUCT, ws_row, ws_col, ws_xpixel, ws_ypixel);
-
 #define IOCTL_FILE "/dev/tty"
 
 
@@ -31,12 +31,46 @@ void print_buffer(void *ptr, size_t len) {
     }
     printf("\n");
 }
-void debug_print(const IOCTL_STRUCT & my_struct) {
-    visit_struct::for_each(my_struct,
-        [](const char * name, const auto & value) {
-            std::cerr << name << ": " << value << std::endl;
-        }
-    );
+
+template <class T>
+void meta_print(const char* name, const T& value) {
+    if constexpr(std::is_same_v<T, char> || std::is_same_v<T, unsigned char>) {
+        std::cout << name << ": " << (int)value;
+    } else {
+        std::cout << name << ": " << value;
+    }
+}
+
+// See: https://github.com/cbeck88/visit_struct/issues/26
+template <class Value>
+void  debug_print(const char* name, const Value& value, int indent = 0) {
+    const std::string INDENT = std::string(indent * 2, ' ');
+    if constexpr (visit_struct::traits::is_visitable<Value>::value) {
+        std::cout << INDENT
+            << name
+            << ": "
+            << "{"
+            << std::endl;
+        visit_struct::for_each(value, [&](const char* name, const auto& member) {
+            debug_print(name, member, indent + 1);
+        });
+        std::cout << INDENT << "}" << std::endl;
+    } else if constexpr (std::is_array_v<Value>) {
+            int i = 0;
+            std::cout << INDENT
+                << name
+                << ": "
+                << "[" << std::endl;
+            for (const auto& element : value) {
+                debug_print(std::to_string(i).c_str(), element, indent + 1);
+                i++;
+            }
+            std::cout << INDENT << "]" << std::endl;;
+    } else {
+        std::cout << INDENT;
+        meta_print(name, value);
+        std::cout << std::endl;
+    }
 }
 
 int main() {
@@ -59,7 +93,7 @@ int main() {
     }
 
     print_buffer(&buffer, sizeof(buffer));
-    debug_print(buffer);
+    debug_print("/", buffer);
 
     return EXIT_SUCCESS;
 }
