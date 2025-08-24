555bcc2e787fa2a05986805875b72391ffbc1f4e
PEMessage <1165739182@qq.com>
2025-08-25 00:22:26 +0800

    aarch64/minimal-qemu: add minimal_chr, add config_rw
    Test:
        insmod minimal_chr.ko
        mdev -s
        echo 123 > /dev/minimal_chr0
        cat /dev/minimal_chr0

--- kernel/aarch64/minimal-qemu/modules/minimal_chr/minimal_chr.c
+++ kernel/aarch64/minimal-qemu/modules/minimal_chr/minimal_chr.c
@@ -30,6 +30,8 @@ int nr = DEFAULT_NR;
 char *name = DEFAULT_NAME;
 
 bool config_sysfs = true;
+bool config_rw = true;
+int config_rw_size = 512;
 
 module_param(major, int, S_IRUGO);
 module_param(minor, int, S_IRUGO);
@@ -37,35 +39,93 @@ module_param(nr, int, S_IRUGO);
 module_param(name, charp, S_IRUGO);
 
 module_param(config_sysfs, bool, S_IRUGO);
+module_param(config_rw, bool, S_IRUGO);
+module_param(config_rw_size, int, S_IRUGO);
 
 dev_t major_minor; // 高12位是主设备号，低20位是次设备号
 
 struct private_dev { // 实际的字符设备结构，类似于面向对象的继承
+        // Kernel Object
         struct cdev cdev;
-        char c;
+
+        // Buffer & mutex [optional]
+        char *buffer;
+        ssize_t size;
+        ssize_t max_size;
+
+
+
 };
 
 // sysfs
 struct class *cls; // add cls
 
 static int do_open(struct inode *inode, struct file *filp) {
+
+    // file->private_data = ...
+    struct private_dev *pdev = container_of(inode->i_cdev, struct private_dev, cdev);
+    filp->private_data = pdev;
+
     printk(KERN_INFO "do open: inode is %d, minor is %d\n", iminor(inode),
            MINOR(inode->i_cdev->dev));
     return 0;
 }
 static ssize_t do_read(struct file *filp, char __user *buf, size_t count,
                        loff_t *f_pos) {
+    struct private_dev *pdev = filp->private_data;
+    ssize_t bytes_to_read = 0;
     printk(KERN_INFO "do read\n");
-    return 0;
+
+    if (!config_rw) {
+        return 0;
+    }
+
+    // ------ config_rw ------
+    // Check if we're at the end of the buffer
+    if (*f_pos >= pdev->size) { return 0; } // EOF
+    bytes_to_read = min(count, (size_t)(pdev->size - *f_pos));
+    if (copy_to_user(buf, pdev->buffer + *f_pos, bytes_to_read)) {
+        return -EFAULT; // Failed to copy to userspace
+    }
+    *f_pos += bytes_to_read;
+
+    /* read will not decrease buffer size(compare to write) */
+
+    return bytes_to_read;
 }
+
 static ssize_t do_write(struct file *filp, const char __user *buf, size_t count,
                         loff_t *f_pos) {
+
+    struct private_dev *pdev = filp->private_data;
+    ssize_t bytes_to_write = 0;
     printk(KERN_INFO "do write\n");
-    return count; // 不能返回0，否则会不停的写
+
+    if (!config_rw) {
+        return count; // 不能返回0，否则会不停的写
+    } 
+
+    // ------ config_rw ------
+    // Check if we're at the end of the buffer
+    if (*f_pos >= pdev->max_size) { return -ENOSPC; } // No space left
+    bytes_to_write = min(count, (size_t)(pdev->max_size - *f_pos));
+    if (copy_from_user(pdev->buffer + *f_pos, buf, bytes_to_write)) {
+        return -EFAULT; // Failed to copy from userspace
+    }
+    *f_pos += bytes_to_write;
+
+    if (*f_pos > pdev->size) {
+        pdev->size = *f_pos;
+    }
+
+    return  bytes_to_write;
 }
 
 static int do_release(struct inode *inode, struct file *filp) {
     printk(KERN_INFO "do release\n");
+
+    /* struct private_dev *pridev = filp->private_data; */
+
     return 0;
 }
 
@@ -83,6 +143,7 @@ struct private_dev *pdev;
 static int __init mc_do_init(void) {
     int ret = 0;
     int i = 0;
+    int j = 0;
     pr_info("Enter mc_do_init\n");
 
     // 1. register dev_t
@@ -144,20 +205,44 @@ static int __init mc_do_init(void) {
         // it gets buffered and may not appear immediately in the kernel log
         pr_info("create sysfs...\n");
         cls = class_create(THIS_MODULE, DEFAULT_NAME);
-        for (i=0; i < nr; i++) {
-            device_create(cls, NULL, MKDEV(major, minor + i), NULL, DEFAULT_NAME "%d", i);
+        for (int x=0; x < nr; x++) {
+            device_create(cls, NULL, MKDEV(major, minor + x), NULL, DEFAULT_NAME "%d", x);
         }
     } else {
         pr_info("config_sysfs disabled: %d\n", config_sysfs);
     }
 
+    // 5. rw[optional]: 
+    // --------------------------------------
+    if (config_rw) {
+        pr_info("create buffer...\n");
+        for (j=0; j < nr; j++) {
+            pdev[j].buffer = kmalloc(config_rw_size, GFP_KERNEL);
+            if (!pdev[j].buffer) {
+                pr_err("Fail to create buffer[%d]\n", j);
+                goto step5_fail;
+            }
+            pdev[j].max_size = config_rw_size;
+            pdev[j].size = 0;
+        }
+    } else {
+        pr_info("config_rw disabled: %d\n", config_rw);
+    }
+
+
 /* success: */
     return 0;
 
+step5_fail:
+    for(int x = 0; x < j; x++) {
+        kfree(pdev[j].buffer);
+    }
+/* step4_fail: */
+    /* pass */
 step3_fail:
     // Clean up all cdevs that were successfully added
-    for (int j = 0; j < i; j++) {
-        cdev_del(&pdev[j].cdev);
+    for (int x = 0; x < i; x++) {
+        cdev_del(&pdev[x].cdev);
     }
     kfree(pdev);
     pdev = NULL;
