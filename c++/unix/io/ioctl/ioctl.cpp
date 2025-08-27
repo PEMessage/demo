4b6f34effc25d4b38570f91a23f7935c490e102b
PEMessage <1165739182@qq.com>
2025-08-27 23:58:41 +0800

    c++/unix/io: ioctl, allow to also print type(not enable)

--- c++/unix/io/ioctl/ioctl.cpp
+++ c++/unix/io/ioctl/ioctl.cpp
@@ -11,6 +11,28 @@
 #include <iostream>
 #include "visit_struct/visit_struct.hpp"
 
+#include <string_view>
+template <typename T>
+constexpr auto type_name() {
+    std::string_view name, prefix, suffix;
+#ifdef __clang__
+    name = __PRETTY_FUNCTION__;
+    prefix = "auto type_name() [T = ";
+    suffix = "]";
+#elif defined(__GNUC__)
+    name = __PRETTY_FUNCTION__;
+    prefix = "constexpr auto type_name() [with T = ";
+    suffix = "]";
+#elif defined(_MSC_VER)
+    name = __FUNCSIG__;
+    prefix = "auto __cdecl type_name<";
+    suffix = ">(void)";
+#endif
+    name.remove_prefix(prefix.size());
+    name.remove_suffix(suffix.size());
+    return name;
+}
+
 VISITABLE_STRUCT(winsize, ws_row, ws_col, ws_xpixel, ws_ypixel);
 
 #define IOCTL_CMD     TIOCGWINSZ
@@ -23,7 +45,7 @@ void print_buffer(void *ptr, size_t len) {
 
     // Print the pointer address in hex (byte by byte)
     printf("Content: \n");
-    for (size_t i = 0; i < sizeof(void*); i++) {
+    for (size_t i = 0; i < len; i++) {
         printf("%02X ", ((unsigned char*)&ptr)[i]);
         if (i % 8 == 8 -1) {
             printf("\n");
@@ -31,6 +53,7 @@ void print_buffer(void *ptr, size_t len) {
     }
     printf("\n");
 }
+
 
 template <class T>
 void meta_print(const char* name, const T& value) {
@@ -40,6 +63,15 @@ void meta_print(const char* name, const T& value) {
         std::cout << name << ": " << value;
     }
 }
+
+template<typename T>
+struct is_char_array : std::false_type {};
+
+template<std::size_t N>
+struct is_char_array<char[N]> : std::true_type {};
+
+template<std::size_t N>
+struct is_char_array<const char[N]> : std::true_type {};
 
 // See: https://github.com/cbeck88/visit_struct/issues/26
 template <class Value>
@@ -54,8 +86,10 @@ void  debug_print(const char* name, const Value& value, int indent = 0) {
         visit_struct::for_each(value, [&](const char* name, const auto& member) {
             debug_print(name, member, indent + 1);
         });
-        std::cout << INDENT << "}" << std::endl;
-    } else if constexpr (std::is_array_v<Value>) {
+        std::cout << INDENT << "}"
+            // << "  // " <<  type_name<Value>()
+            << std::endl;
+    } else if constexpr (std::is_array_v<Value> && !std::is_same_v<std::remove_extent_t<Value>, char>) {
             int i = 0;
             std::cout << INDENT
                 << name
@@ -65,7 +99,9 @@ void  debug_print(const char* name, const Value& value, int indent = 0) {
                 debug_print(std::to_string(i).c_str(), element, indent + 1);
                 i++;
             }
-            std::cout << INDENT << "]" << std::endl;;
+            std::cout << INDENT << "]"
+                // << "  // " <<  type_name<Value>()
+                << std::endl;;
     } else {
         std::cout << INDENT;
         meta_print(name, value);
@@ -75,7 +111,7 @@ void  debug_print(const char* name, const Value& value, int indent = 0) {
 
 int main() {
     int fd;
-    IOCTL_STRUCT buffer = {0};  // Zero-initialized
+    IOCTL_STRUCT buffer {};
 
     // We use stdout (or any TTY) as the file descriptor for terminal ioctls
     printf("Using file: %s\n", IOCTL_FILE);
