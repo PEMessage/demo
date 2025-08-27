ac4f0e0e9ac047d500ab87c76af423bdd09e007a
PEMessage <1165739182@qq.com>
2025-08-28 02:16:39 +0800

    c++/unix/io: ioctl, allow change key value through cmdline, and fix print_buffer

--- c++/unix/io/ioctl/ioctl.cpp
+++ c++/unix/io/ioctl/ioctl.cpp
@@ -9,6 +9,9 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
 #include "visit_struct/visit_struct.hpp"
 
 #include <string_view>
@@ -44,14 +47,14 @@ void print_buffer(void *ptr, size_t len) {
     unsigned char *addr = (unsigned char *)ptr;
 
     // Print the pointer address in hex (byte by byte)
-    printf("Content: \n");
+    printf("Content: %p\n", ptr);
     for (size_t i = 0; i < len; i++) {
-        printf("%02X ", ((unsigned char*)&ptr)[i]);
+        printf("%02X ", ((unsigned char*)ptr)[i]);
         if (i % 8 == 8 -1) {
             printf("\n");
         }
     }
-    printf("\n");
+    // printf("\n");
 }
 
 
@@ -109,20 +112,81 @@ void  debug_print(const char* name, const Value& value, int indent = 0) {
     }
 }
 
-int main() {
+std::map<std::string, std::string> parse_key_value_args(int argc, char* argv[]) {
+    std::map<std::string, std::string> kv_pairs;
+
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        size_t pos = arg.find('=');
+        if (pos != std::string::npos) {
+            std::string key = arg.substr(0, pos);
+            std::string value = arg.substr(pos + 1);
+            kv_pairs[key] = value;
+        }
+    }
+
+    return kv_pairs;
+}
+
+
+template<typename T>
+T string_to_value(const std::string& str) {
+    std::istringstream iss(str);
+    T value;
+    iss >> value;
+    return value;
+}
+
+// Specialization for char (to handle numeric conversion)
+template<>
+char string_to_value<char>(const std::string& str) {
+    return static_cast<char>(std::stoi(str));
+}
+
+// Specialization for unsigned char
+template<>
+unsigned char string_to_value<unsigned char>(const std::string& str) {
+    return static_cast<unsigned char>(std::stoul(str));
+}
+
+
+template<typename T>
+void modify_struct_fields(T& struct_instance, const std::map<std::string, std::string>& kv_pairs) {
+    visit_struct::for_each(struct_instance, [&](const char* name, auto& member) {
+        std::string field_name(name);
+        auto it = kv_pairs.find(field_name);
+        if (it != kv_pairs.end()) {
+            using MemberType = std::decay_t<decltype(member)>;
+            try {
+                member = string_to_value<MemberType>(it->second);
+                std::cout << "Set " << field_name << " = " << it->second << std::endl;
+            } catch (const std::exception& e) {
+                std::cerr << "Error converting value for " << field_name
+                          << ": " << e.what() << std::endl;
+            }
+        }
+    });
+}
+
+
+int main(int argc, char *argv[]) {
+    // Part1 Parse config
+    auto kv_pairs = parse_key_value_args(argc, argv);
+
     int fd;
     IOCTL_STRUCT buffer {};
 
     // We use stdout (or any TTY) as the file descriptor for terminal ioctls
+    std::cout << "---------------------------" << std::endl;
     printf("Using file: %s\n", IOCTL_FILE);
     fd =  open(IOCTL_FILE, O_RDONLY);
 
     printf("Using file descriptor: %d\n", fd);
+    std::cout << "---------------------------" << std::endl;
 
     // Perform ioctl to get window size
     int ret = ioctl(fd, IOCTL_CMD, &buffer);
     printf("ioctl return value: %d\n", ret);
-
     if (ret < 0) {
         perror("ioctl IOCTL_CMD failed");
         return EXIT_FAILURE;
@@ -130,6 +194,16 @@ int main() {
 
     print_buffer(&buffer, sizeof(buffer));
     debug_print("/", buffer);
+
+    // Modify struct based on command line arguments before ioctl
+    if (!kv_pairs.empty()) {
+        std::cout << "---------------------------" << std::endl;
+        std::cout << "Modifying struct with key-value pairs:" << std::endl;
+        modify_struct_fields(buffer, kv_pairs);
+        std::cout << "---------------------------" << std::endl;
+        print_buffer(&buffer, sizeof(buffer));
+        debug_print("/", buffer);
+    }
 
     return EXIT_SUCCESS;
 }
