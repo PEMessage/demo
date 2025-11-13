0c28a2d51b2e5cb4b33c638c24aaeccd0bdf7550
PEMessage <1165739182@qq.com>
2025-11-14 00:00:07 +0800

    c++/gui/gtk: also print display name

--- c++/gui/gtk/event_spy/event_spy.cpp
+++ c++/gui/gtk/event_spy/event_spy.cpp
@@ -3,9 +3,8 @@
 #include <magic_enum.hpp>
 
 // q-gcc: `pkg-config --cflags --libs gtkmm-3.0` -I. --
-int main()
+int main(int argc, char *argv[])
 {
-
     // Print GTK version
     std::cout << "GTK+ version: " << GTK_MAJOR_VERSION << "."
         << GTK_MINOR_VERSION << "." << GTK_MICRO_VERSION << std::endl;
@@ -15,6 +14,15 @@ int main()
         << GTKMM_MINOR_VERSION << "." << GTKMM_MICRO_VERSION << std::endl;
 
     auto app = Gtk::Application::create();
+
+    auto display = Gdk::Display::get_default();
+    if (display) {
+        std::cout << "GDK backend: " << display->get_name() << std::endl;
+    } else {
+        std::cout << "GDK backend: No display available" << std::endl;
+    }
+
+
     Gtk::Window window;
     window.set_default_size(1024, 768);
     app->signal_startup().connect([&]
