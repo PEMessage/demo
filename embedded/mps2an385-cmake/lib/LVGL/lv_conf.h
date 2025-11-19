a4e60c3e28bdf7709cd8a65eb9b2567b2545aaf4
PEMessage <1165739182@qq.com>
2025-11-19 23:21:53 +0800

    embedded/mps2an385-cmake: enable LV_USE_GESTURE_RECOGNITION for multitouch

--- embedded/mps2an385-cmake/lib/LVGL/lv_conf.h
+++ embedded/mps2an385-cmake/lib/LVGL/lv_conf.h
@@ -523,7 +523,7 @@
 
 /* Enable the multi-touch gesture recognition feature */
 /* Gesture recognition requires the use of floats */
-#define LV_USE_GESTURE_RECOGNITION 0
+#define LV_USE_GESTURE_RECOGNITION 1
 
 /*=====================
  *  COMPILER SETTINGS
@@ -566,7 +566,7 @@
 #define LV_ATTRIBUTE_EXTERN_DATA
 
 /** Use `float` as `lv_value_precise_t` */
-#define LV_USE_FLOAT            0
+#define LV_USE_FLOAT            1
 
 /** Enable matrix support
  *  - Requires `LV_USE_FLOAT = 1` */
