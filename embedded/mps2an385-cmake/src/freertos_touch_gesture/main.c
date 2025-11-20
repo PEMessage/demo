d99c32409d2f412e3c52fbe70f9dfb5996b5db76
PEMessage <1165739182@qq.com>
2025-11-20 19:42:42 +0800

    embedded/mps2an385-cmake: add freertos_touch_gesture, allow both way to trigger InputDeviceScan

---
+++ embedded/mps2an385-cmake/src/freertos_touch_gesture/main.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include "CMSDK_CM3.h" // for SystemCoreClock
+#include "CMSDK_CM3_EXT.h" // for SystemCoreClock
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+void NVIC_Init() {
+    size_t i = 0 ;
+    for ( i = 0; i < 32; i++) {
+        // NVIC_DisableIRQ(i);
+        NVIC_SetPriority((IRQn_Type)i, configMAX_SYSCALL_INTERRUPT_PRIORITY + 1);
+    }
+}
+
+void TouchIRQ_Init() {
+    TOUCH_CTRL->enable_irq = 1;
+    TOUCH_CTRL->reserved = 77; // random number to verify struct work
+    NVIC_EnableIRQ(Touch_IRQn);
+}
+
+
+void setup() {
+    extern int stdout_init (void);
+    stdout_init();
+    NVIC_Init();
+    TouchIRQ_Init();
+}
+
+// ========================================
+// Input Framework
+// ========================================
+
+
+typedef struct InputDevice {
+} InputDevice;
+
+struct InputDevice *DEV;
+
+void InputDeviceScan(InputDevice* indev) {
+    if (!indev) { return;}
+    printf("Timer triggered!\n");
+}
+
+
+// ========================================
+// Adapter
+// ========================================
+
+// Call From IRQ
+// ----------------------------------------
+void touch_pend_callback(void * arg1, uint32_t arg2) {
+    struct InputDevice *indev = (InputDevice *)arg1;
+    InputDeviceScan(indev);
+}
+
+void TouchIRQ_Handler() {
+    static uint32_t prev_pressed = 0;
+    // Edge detect
+    if (prev_pressed == *TOUCH_PRESS) { return; }
+    // Only pressed down trigger
+    if (*TOUCH_PRESS == 0) { return; }
+
+    // See:
+    // https://freertos.org/zh-cn-cmn-s/Documentation/02-Kernel/04-API-references/11-Software-timers/18-xTimerPendFunctionCallFromISRhttps://freertos.org/zh-cn-cmn-s/Documentation/02-Kernel/04-API-references/11-Software-timers/18-xTimerPendFunctionCallFromISR
+    BaseType_t xHigherPriorityTaskWoken;
+    xTimerPendFunctionCallFromISR(
+            touch_pend_callback,
+            DEV,
+            NULL,
+            &xHigherPriorityTaskWoken
+            );
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+
+// Call From Timer
+// ----------------------------------------
+const uint32_t PERIOD = 33; // ms
+static TimerHandle_t touch_timer = NULL;
+void touch_timer_callback(TimerHandle_t xTimer) {
+    printf("Tick count: %u ", xTaskGetTickCount());
+    InputDeviceScan(DEV);
+}
+
+
+void MainTask() {
+    // create indev
+    DEV = malloc(sizeof(*DEV));
+
+    // Create a timer (initially not active)
+    touch_timer = xTimerCreate(
+        "TouchTimer",
+        pdMS_TO_TICKS(PERIOD),  // 1 second period
+        pdTRUE,               // Auto-reload
+        NULL,                 // Timer ID
+        touch_timer_callback
+    );
+
+    // Start the timer
+    xTimerStart(touch_timer, portMAX_DELAY);
+
+    while(1) {
+    }
+}
+
+int main() {
+    setup();
+    printf("Init complete\n");
+
+    xTaskCreate( MainTask,
+            "MainTask",
+            configMINIMAL_STACK_SIZE + 3*1024,
+            NULL,
+            (tskIDLE_PRIORITY + 1),
+            NULL );
+
+
+    vTaskStartScheduler();
+    while(1);
+}
