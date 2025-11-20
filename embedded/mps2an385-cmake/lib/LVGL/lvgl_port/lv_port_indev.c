ff335b52c0d291120788ce7c10c58b992a6632f0
PEMessage <1165739182@qq.com>
2025-11-20 23:50:48 +0800

    embedded/mps2an385-cmake: add freertos_touch_gesture, try to add IRQMode(EventMode) for indev_port, but HardFault

--- embedded/mps2an385-cmake/lib/LVGL/lvgl_port/lv_port_indev.c
+++ embedded/mps2an385-cmake/lib/LVGL/lvgl_port/lv_port_indev.c
@@ -17,6 +17,23 @@
  *********************/
 #include "CMSDK_CM3_EXT.h"
 
+
+#define TIMER_MODE 0
+#define EVENT_MODE 1
+
+#ifndef LV_PORT_INDEV_MODE
+    #define LV_PORT_INDEV_MODE TIMER_MODE
+    #define LV_INDEV_MODE  LV_INDEV_MODE_TIMER
+
+#else
+    #define LV_PORT_INDEV_MODE EVENT_MODE
+    #define LV_INDEV_MODE  LV_INDEV_MODE_EVENT
+
+    #include "FreeRTOS.h"
+
+    static void touch_pend_callback(void *dev, uint32_t event); // Added for event mode
+#endif
+
 /**********************
  *      TYPEDEFS
  **********************/
@@ -54,6 +71,8 @@ void lv_port_indev_init(void)
 
     /*Register a touchpad input device*/
     indev_touchpad = lv_indev_create();
+    lv_indev_set_mode(indev_touchpad, LV_INDEV_MODE);
+
     lv_indev_set_type(indev_touchpad, LV_INDEV_TYPE_POINTER);
     lv_indev_set_read_cb(indev_touchpad, touchpad_read);
 
@@ -69,9 +88,12 @@ void lv_port_indev_init(void)
  * -----------------*/
 
 /*Initialize your touchpad*/
-static void touchpad_init(void)
-{
-    /*Nothing to initialize for memory-mapped touch controller*/
+static void touchpad_init(void) {
+#if LV_PORT_INDEV_MODE == EVENT_MODE
+    TOUCH_CTRL->enable_irq = 1;
+    TOUCH_CTRL->reserved = 77; // random number to verify struct work
+    NVIC_EnableIRQ(Touch_IRQn);
+#endif
 }
 
 /*Will be called by the library to read the touchpad*/
@@ -108,6 +130,49 @@ static void touchpad_get_xy(int32_t * x, int32_t * y)
     *x = (int32_t)(*TOUCH_X);
     *y = (int32_t)(*TOUCH_Y);
 }
+
+
+
+
+/*------------------
+ * Touchpad Event Mode Extra
+ * -----------------*/
+#if LV_PORT_INDEV_MODE == EVENT_MODE
+/* Event mode callback function - called from ISR via FreeRTOS */
+
+void touch_pend_callback(void * arg1, uint32_t arg2) {
+    struct lv_indev_t *indev = (lv_indev_t *)arg1;
+    lv_indev_read(indev);
+}
+/* Touch Interrupt Handler */
+void TouchIRQ_Handler(void) {
+    static uint32_t prev_pressed = 0;
+    uint32_t prev = prev_pressed;
+    uint32_t curr = *TOUCH_PRESS;
+    prev_pressed = curr; // update prev
+
+    #define NO_HOVER_SUPPORT
+    #ifdef NO_HOVER_SUPPORT
+    // if neither edge nor pressed skip it -- skip hover event
+    if (!( (prev ^ curr) || curr )) {
+        return;
+    }
+    #endif
+
+    // Defer the touch processing to the RTOS task context
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xTimerPendFunctionCallFromISR(
+            touch_pend_callback,
+            indev_touchpad,  // No specific device context needed
+            0,     // No event parameter needed
+            &xHigherPriorityTaskWoken
+            );
+
+    // Perform context switch if necessary
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+#endif
 
 #else /*Enable this file at the top*/
 
