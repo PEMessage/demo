deb46d710f5adef623fbe3509da3a758344937cb
PEMessage <1165739182@qq.com>
2025-11-27 00:22:28 +0800

    c++/unix/io: ioctl, improve build script

--- c++/unix/io/ioctl/build.sh
+++ c++/unix/io/ioctl/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# cmake -DCMAKE_TOOLCHAIN_FILE=/opt/android-sdk/ndk/21.0.6113669/build/cmake/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a -B build_arm &&
-#     cmake --build build_arm
+
+if [ -n "$ANDROID_HOME" ] && [ -f "$ANDROID_HOME/ndk/21.0.6113669/build/cmake/android.toolchain.cmake" ] ; then
+    cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/ndk/21.0.6113669/build/cmake/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a -B build_arm &&
+        cmake --build build_arm
+fi
 
 cmake  -B build &&
     cmake --build build
