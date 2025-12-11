6c21af3e959a55c5eb402ba54b4d6912492e57ad
PEMessage <1165739182@qq.com>
2025-12-12 00:10:50 +0800

    embedded/mps2an385-cmake: add freertos_golang_channel, add first implement but have issue if send data to channel if already being sent

--- embedded/mps2an385-cmake/src/freertos_golang_channel/main.c
+++ embedded/mps2an385-cmake/src/freertos_golang_channel/main.c
@@ -2,6 +2,7 @@
 #include "CMSDK_CM3.h" // for SystemCoreClock
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 void NVIC_Init() {
     size_t i = 0 ;
@@ -17,37 +18,160 @@ void setup() {
     NVIC_Init();
 }
 
-void TaskPing() {
-    while(1) {
-        printf("Ping!\n");
-        vTaskDelay(300);
+// ==============================================
+
+typedef struct {
+    SemaphoreHandle_t data_ready;  // Receiver waits on this
+    SemaphoreHandle_t data_taken;  // Sender waits on this
+    void* data;                    // Shared data pointer
+} Channel;
+
+// Initialize channel
+Channel* channel_create(void) {
+    Channel* ch = pvPortMalloc(sizeof(Channel));
+    if (ch == NULL) return NULL;
+
+    ch->data_ready = xSemaphoreCreateBinary();
+    ch->data_taken = xSemaphoreCreateBinary();
+    ch->data = NULL;
+
+    return ch;
+}
+
+// Send data (blocks until receiver takes it)
+BaseType_t channel_send(Channel* ch, void* data, TickType_t timeout) {
+    if (ch == NULL) return pdFALSE;
+
+    // Store data
+    ch->data = data;
+
+    // Signal receiver that data is ready
+    xSemaphoreGive(ch->data_ready);
+
+    // Wait for any previous data to be taken
+    if (xSemaphoreTake(ch->data_taken, timeout) != pdTRUE) {
+        return pdFALSE;  // Timeout
     }
+
+
+    return pdTRUE;
+}
+
+// Receive data (blocks until sender sends)
+BaseType_t channel_recv(Channel* ch, void** data_ptr, TickType_t timeout) {
+    if (ch == NULL || data_ptr == NULL) return pdFALSE;
+
+    // Wait for data to be ready
+    if (xSemaphoreTake(ch->data_ready, timeout) != pdTRUE) {
+        return pdFALSE;  // Timeout
+    }
+
+    // Get data
+    *data_ptr = ch->data;
+    ch->data = NULL;
+
+    // Signal sender that data has been taken
+    xSemaphoreGive(ch->data_taken);
+
+    return pdTRUE;
+}
+
+// Try to receive without blocking
+BaseType_t channel_try_recv(Channel* ch, void** data_ptr) {
+    return channel_recv(ch, data_ptr, 0);
+}
+
+// Try to send without blocking
+BaseType_t channel_try_send(Channel* ch, void* data) {
+    return channel_send(ch, data, 0);
+}
+
+// Destroy channel
+void channel_destroy(Channel* ch) {
+    if (ch) {
+        vSemaphoreDelete(ch->data_ready);
+        vSemaphoreDelete(ch->data_taken);
+        vPortFree(ch);
+    }
+}
+
+// ==============================================
+Channel *ch;
+Channel *end;
+
+void TaskSend() {
+    while(1) {
+
+        printf("Send Start\n");
+        channel_send(ch, NULL, portMAX_DELAY);
+        printf("Send End\n");
+
+        channel_send(end, NULL, portMAX_DELAY);
+        printf("Send Final\n");
+
+        while(1);
+
+    }
+}
+
+void TaskRecv() {
+    while(1) {
+
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        printf("Recv Start\n");
+        void *data;
+        channel_recv(ch, &data, portMAX_DELAY);
+        printf("Recv End\n");
+
+        channel_send(end, NULL, portMAX_DELAY);
+        printf("Recv Final\n");
+
+        while(1);
+    }
+}
+
+void TaskMain() {
+    ch = channel_create();
+    end = channel_create();
+
+    xTaskCreate( TaskSend,
+            "TaskSend",
+            configMINIMAL_STACK_SIZE + 3*1024,
+            NULL,
+            (tskIDLE_PRIORITY + 1),
+            NULL );
+    xTaskCreate( TaskRecv,
+            "TaskRecv",
+            configMINIMAL_STACK_SIZE + 3*1024,
+            NULL,
+            (tskIDLE_PRIORITY + 1),
+            NULL );
+
+
+    for (int i = 0; i < 2 ; i++) {
+        void* data;
+
+        channel_recv(end, &data, portMAX_DELAY);
+    }
+    printf("End !!\n");
+    while(1);
 
 }
 
-void TaskPong() {
-    while(1) {
-        printf("Pong!\n");
-        vTaskDelay(40);
-    }
-
-}
 
 int main() {
     setup();
     printf("Init complete\n");
-    xTaskCreate( TaskPing,
-            "TaskPing",
+
+
+    xTaskCreate( TaskMain,
+            "TaskMain",
             configMINIMAL_STACK_SIZE + 3*1024,
             NULL,
             (tskIDLE_PRIORITY + 1),
             NULL );
-    xTaskCreate( TaskPong,
-            "TaskPong",
-            configMINIMAL_STACK_SIZE + 3*1024,
-            NULL,
-            (tskIDLE_PRIORITY + 1),
-            NULL );
+
     vTaskStartScheduler();
     while(1);
 }
