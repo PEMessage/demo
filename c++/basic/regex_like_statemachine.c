a586afdaa5130cbee29a820de3d414162cf1a3a4
PEMessage <1165739182@qq.com>
2025-12-15 23:29:20 +0800

    c++/basic: add regex_like_statemachine.c

---
+++ c++/basic/regex_like_statemachine.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+// 【如果直接用 C 语言替代正则表达式会怎样 | Tsoding Daily】 【精准空降到 12:55】
+// https://www.bilibili.com/video/BV1MrmjBjEeg/?share_source=copy_web&vd_source=ba726eaf572f03aca4ba3d79f0118159&t=775
+
+bool is_huid(const char *s)
+{
+    // similiar to '{0-9}[8]-{0-9}[6]'
+    for (size_t i = 0; i < 8; ++i) if (!isdigit(*s++)) return false;
+    if (*s++ != '-') return false;
+    for (size_t i = 0; i < 6; ++i) if (!isdigit(*s++)) return false;
+    return true;
+}
+
+#define HUID_TEST_CASE(huid) printf("%s => %s\n", (huid), is_huid(huid) ? "true" : "false");
+
+int main()
+{
+    HUID_TEST_CASE("20250906-014343");
+    HUID_TEST_CASE("HelloWorld");
+    HUID_TEST_CASE("20251011-061535");
+    HUID_TEST_CASE("20251a11-061535");
+    return 0;
+}
