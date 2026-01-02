0e3a1880117cd8c2411b764ed3c1ae8e5cfef4b0
PEMessage <1165739182@qq.com>
2026-01-03 03:31:06 +0800

    c++/gui/glfw: add helloworld.cpp, create windows and display opengl version

--- c++/gui/glfw/basic/helloworld.cpp
+++ c++/gui/glfw/basic/helloworld.cpp
@@ -1,17 +1,39 @@
 // q-gcc: `pkg-config glfw3 gl --cflags --libs` --
+//
 // Thanks to: https://discourse.glfw.org/t/compiling-a-glfw-demo-file-without-glad/1682/2
 // we don't use GLAD at linux
 
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <assert.h>
 
 
 int main()
 {
     // glfw: initialize and configure
     // ------------------------------
-    glfwInit();
+    assert(glfwInit());
+    // set opengl version 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // set opengl to Core-profile(not Immediate-mode)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // NOTE: MacOS need this line
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    assert(window);
+    glfwMakeContextCurrent(window);
+
+
+    std::cout << "version: " << (const char*)glGetString(GL_VERSION) << std::endl;
+    getchar();
 }
