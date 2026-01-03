e285fff11da3627f0a59dd6ef702d036ea942381
PEMessage <1165739182@qq.com>
2026-01-04 00:56:28 +0800

    web/basic: add canvas example, print a point at center

--- web/basic/canvas/index.js
+++ web/basic/canvas/index.js
@@ -7,6 +7,10 @@
 /** @type {HTMLCanvasElement} */
 const game = document.getElementById('game');
 
+const BACKGROUND = "#101010"
+const FOREGROUND = "#50FF50"
+
+
 console.log(game)
 game.width = 800
 game.height = 800
@@ -14,16 +18,25 @@ game.height = 800
 const ctx = game.getContext("2d") // similiar to glfwMakeContextCurrent?
 console.log(ctx)
 
-// background
-{
-    ctx.fillStyle = "#101010"
+function clear(){
+    ctx.fillStyle = BACKGROUND
     ctx.fillRect(0, 0, game.width, game.height)
 }
 
-// content
-{
-    // ctx.fillStyle = "green"
-    ctx.fillStyle = "#50FF50"
-    ctx.fillRect(0, 0, 100, 100)
+function point({x, y}) {
+    const s = 20
+    ctx.fillStyle = FOREGROUND
+    ctx.fillRect(x - s/2, y - s/2, s, s)
 }
+
+// convert to screen position
+function screen(p) {
+    return {
+        x: (p.x + 1) / 2 * game.width,
+        y: (p.y + 1) / 2 * game.height
+    }
+}
+
+clear()
+point(screen({x: 0, y: 0}))
 
