d9d23c50dba423a56f482a2fbc75f316c8bcfe7a
PEMessage <1165739182@qq.com>
2026-01-03 23:44:16 +0800

    c++/gui/glfw: helloworld_triangle, Duplicate helloworld/helloworld.cpp history in helloworld_triangle/helloworld_triangle.cpp history.

---
+++ c++/gui/glfw/basic/helloworld_triangle/helloworld_triangle.cpp
@@ -0,0 +1,110 @@
+// q-gcc: `pkg-config glfw3 gl --cflags --libs` -std=c++2b --
+//
+// Thanks to: https://discourse.glfw.org/t/compiling-a-glfw-demo-file-without-glad/1682/2
+// we don't use GLAD at linux
+
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <assert.h>
+
+
+int main()
+{
+    // 1. glfw: initialize and configure
+    // ------------------------------
+    assert(glfwInit());
+    // set opengl version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // set opengl to Core-profile(not Immediate-mode)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // NOTE: MacOS need this line
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    // 2. glfw: window creation
+    //    opengl: let window become current opengl context
+    /// -------------------------------------------
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    assert(window);
+    // NOTICE: opengl API must be called after this line
+    glfwMakeContextCurrent(window);
+    std::cout << "version: " << (const char*)glGetString(GL_VERSION) << std::endl; // simplest opengl api
+
+
+    // 3. Set callback winsize change: If windows size change `glViewport` reset it
+    /// -------------------------------------------
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    // 4. render loop
+    // -----------
+    while(!glfwWindowShouldClose(window))
+    {
+        // input
+        void processInput(GLFWwindow *window);
+        processInput(window);
+
+        // 5. put you render cmd here
+        void render();
+        render();
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate(); // ASAN will compliant, let it go
+    return 0;
+}
+
+
+void render() {
+    // 在每个新的渲染迭代开始的时候我们总是希望清屏，否则我们仍能看见上一次迭代的渲染结果
+    // if not set, will use BLACK
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // glClear: take a `buffer bit` as arguement
+    //          optional arg: GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_STENCIL_BUFFER_BIT
+    // every bit represent a PIXEL buffer
+    // ColorBuffer[width][height] = { {Pixel(R,G,B,A), Pixel(R,G,B,A), ...}
+    // DepthBuffer[width][height] = { z, z, ... }                           深度值（通常0.0-1.0的浮点数）
+    // StencilBuffer[width][height] = { stencil, stencil, ... }
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+
+// On ESC Pressed
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // Provide argument to 'fragment shader'(render color)
+    // which will do
+    //  From: Normalized Device Coordinates(NDC):
+    //  to:   Screen-space Coordinates:
+       // NDC Space                   Screen Space
+       // [-1, 1]×[-1, 1]             [0, width]×[0, height]
+       //
+       // (-1, 1) ─────── (1, 1)      (0, 0) ─────── (width, 0)
+       //    │               │           │               │
+       //    │     (0, 0)    │           │  (w/2, h/2)   │
+       //    │       •       │   --->    │      •        │
+       //    │               │           │               │
+       // (-1, -1) ────── (1, -1)    (0, height) ─── (width, height)
+       //
+    glViewport(0, 0, width, height);
+}
+
