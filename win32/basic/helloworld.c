da2bf59c6a24ecf124f21067205396e072f4869e
PEMessage <1165739182@qq.com>
2026-01-18 22:56:59 +0800

    win32: add helloworld

---
+++ win32/basic/helloworld.c
@@ -0,0 +1,9 @@
+// cl.exe main.cpp user32.lib
+#include <windows.h>
+
+WINAPI int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
+    return 0;
+}
