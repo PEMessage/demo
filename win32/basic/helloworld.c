78601759ca96c92a27da701fe4d41f15e69879e9
PEMessage <1165739182@qq.com>
2026-01-18 23:04:07 +0800

    win32: add helloworld, some commnet about WinMain arguement

--- win32/basic/helloworld.c
+++ win32/basic/helloworld.c
@@ -1,9 +1,18 @@
 // cl.exe main.cpp user32.lib
 #include <windows.h>
+#include <assert.h>
+#include <stdio.h>
 
-WINAPI int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+// HINSTANCE hInstance: handle to current exe
+// HINSTANCE hPrevInstance: always NULL in win32
+// LPSTR lpCmdLine: cmdline args(not include program name)
+// int nCmdShow: pass to `ShowWindow`
+//
+// `WINAPI` must follow after `return type declare`
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
+    assert(hPrevInstance == NULL);
     MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
     return 0;
 }
