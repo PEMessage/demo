9b4a33ae3426b5549e8eb1f148bebac21a6dd310
PEMessage <1165739182@qq.com>
2026-01-20 00:46:05 +0800

    web/wasm/helloworld: passing argv from js to c

--- web/wasm/helloworld/index.js
+++ web/wasm/helloworld/index.js
@@ -20,24 +20,64 @@ function readString(memory, offset) {
     return new TextDecoder("utf-8").decode(bytes);
 }
 
+function writeString(memory, offset, str) {
+    const mem = new Uint8Array(memory.buffer);
+    const bytes = new TextEncoder("utf-8").encode(str);
+    mem.set(bytes, offset);
+    mem[offset + bytes.length] = 0;
+    return bytes.length + 1;
+}
+
+
+function setup_argc_argv(memory, jsruntimePtr, args) {
+    const argc = args.length;
+    const argvPtrs = [];
+    let currentOffset = jsruntimePtr;
+
+    // Write each argument string to memory
+    for (const arg of args) {
+        const bytesWritten = writeString(memory, currentOffset, arg);
+        argvPtrs.push(currentOffset);
+        currentOffset += bytesWritten;
+    }
+
+    // Create argv array
+    // Align currentOffset to nearest multiple of 4
+    const argvArrayOffset = (currentOffset + 3) & ~3;
+    const argvArray = new Uint32Array(memory.buffer, argvArrayOffset, argc + 1);
+
+    for (let i = 0; i < argc; i++) {
+        argvArray[i] = argvPtrs[i];
+    }
+    argvArray[argc] = 0; // NULL terminator
+
+    return { argc, argvArrayOffset };
+}
+
 /**
  * @param {string} path - The path to the WebAssembly (.wasm) file
  */
 function runWasm(path) {
-    let w = null
+    let wasmInstance = null
 
     const libjs = {
         "console_log": (strPtr) => {
-            // strPtr is the pointer to the string in WebAssembly memory
-            const str = readString(w.instance.exports.memory, strPtr);
+            const str = readString(wasmInstance.exports.memory, strPtr);
             console.log(str);
         },
-    }
+    };
 
     WebAssembly.instantiateStreaming(fetch(path), {
         "env": make_environment(libjs)
     }).then(wasm => {
-        w = wasm // attach w actual wasm handle, allow console_log to reference
-        wasm.instance.exports.main(0, 0)
-    })
+        wasmInstance = wasm.instance;
+        const memory = wasmInstance.exports.memory;
+        const jsruntimePtr = wasmInstance.exports.jsruntime;
+
+        const args = [path, "arg1", "arg2", "arg3"];
+        const { argc, argvArrayOffset } = setup_argc_argv(memory, jsruntimePtr, args);
+
+        // Call main function
+        const result = wasmInstance.exports.main(argc, argvArrayOffset);
+    });
 }
--- web/wasm/helloworld/main.c
+++ web/wasm/helloworld/main.c
@@ -1,12 +1,24 @@
 extern void console_log(const char* str);
 
 #define UNUSED(x) (void)(x)
+#define KEEP __attribute__((used, retain))
 
-__attribute__((export_name("main")))
-int main(int argc, char *argv[])
-{
-    UNUSED(argc);
-    UNUSED(argv);
+
+#define EXPORT(name) __attribute__((export_name(name)))
+
+// TODO: move this part to js
+// align RangeError: start offset of Uint32Array should be a multiple of 4
+KEEP char jsruntime[1024];
+
+EXPORT("main") int main(int argc, char *argv[]) {
+    // UNUSED(argc);
+    // UNUSED(argv);
     console_log("Hello from WebAssembly!");
+
+    // Print all arguments
+    for (int i = 0; i < argc; i++) {
+        console_log(argv[i]);
+    }
+
     return 0;
 }
