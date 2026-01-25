02d16d863964b1a5a2341c64d2f3b9f6fdcde792
PEMessage <1165739182@qq.com>
2026-01-26 00:38:02 +0800

    win32: add helloworld, unicode version

--- win32/basic/helloworld.c
+++ win32/basic/helloworld.c
@@ -1,4 +1,9 @@
 // cl.exe main.cpp user32.lib
+
+// See: https://learn.microsoft.com/en-us/windows/win32/learnwin32/working-with-strings
+//      alwyas define both of them
+#define UNICODE
+#define _UNICODE
 #include <windows.h>
 #include <assert.h>
 #include <stdio.h>
@@ -9,6 +14,10 @@
 // int nCmdShow: pass to `ShowWindow`
 //
 // `WINAPI` must follow after `return type declare`
+
+#ifndef UNICODE
+// NOTICE: we could still use WinMain at UNICODE program, use GetCommandLine
+// to get unicode version of cmdline args
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     LPSTR lpCmdLine, int nCmdShow)
 {
@@ -16,3 +25,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     MessageBox(NULL, "Goodbye, cruel world!", "Note", MB_OK);
     return 0;
 }
+#else
+// See: https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPWSTR lpCmdLine, int nCmdShow)
+{
+    assert(hPrevInstance == NULL);
+    MessageBox(NULL, L"Goodbye, cruel world!", L"Note", MB_OK);
+    return 0;
+}
+#endif
