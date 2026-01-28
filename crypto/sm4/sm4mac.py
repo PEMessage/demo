d6bce451b50b7508c3ba8d1c0e687681a2048318
PEMessage <1165739182@qq.com>
2026-01-29 00:23:42 +0800

    crypto/sm4: init sm4mac.py, see detail for uv cmdline
    Detail:
        uv init --script sm4mac.py
        uv add cryptography --script sm4mac.py

---
+++ crypto/sm4/sm4mac.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "cryptography",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from sm4mac.py!")
+
+
+if __name__ == "__main__":
+    main()
