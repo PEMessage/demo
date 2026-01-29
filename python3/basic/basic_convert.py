7de79ea04ae973077471bdec6d99a5d3e6d2ea80
PEMessage <1165739182@qq.com>
2026-01-30 00:30:38 +0800

    python3/basic: add basic_convert.py, more convert

--- python3/basic/basic_convert.py
+++ python3/basic/basic_convert.py
@@ -8,13 +8,31 @@ import pysnooper
 
 @pysnooper.snoop()
 def int_to():
+    # -> to bytes
     res = int.to_bytes(0)
     res = int.to_bytes(0xDEADBEEF, 4, 'big')
     res = int.to_bytes(0xDEADBEEF, 4, 'little')
 
+@pysnooper.snoop()
+def byte_to():
+    # -> hex string
+    res = b'\x11\x22\xDE'.hex().upper()
+
+
+@pysnooper.snoop()
+def string_to():
+    # -> raw byte
+    res = "random text is fine".encode(encoding='ascii')
+
+
+
+
+
 
 def main() -> None:
     int_to()
+    byte_to()
+    string_to()
 
 
 if __name__ == "__main__":
