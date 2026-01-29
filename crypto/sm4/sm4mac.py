f2f5640b14eecc965887d87328eb5950c2ca16a0
PEMessage <1165739182@qq.com>
2026-01-30 00:07:40 +0800

    crypto/sm4: add more padding variant(maybe not standard)

--- crypto/sm4/sm4mac.py
+++ crypto/sm4/sm4mac.py
@@ -7,18 +7,54 @@
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+def repeat_append_singlebyte_padding(data: bytes = b'', padder: bytes = b'\x00', block_size: int = 16) -> bytes:
+    if len(padder) != 1:
+        raise ValueError("Padder must be a single byte")
+
+    data_len = len(data)
+    remainder = data_len % block_size
+
+    if remainder == 0:
+        return data + (padder * block_size)
+    else:
+        padding_len = block_size - remainder
+        return data + (padder * padding_len)
+
+def gen_pkcs7_like_padding(padder):
+    def func(data):
+        return repeat_append_singlebyte_padding(data, padder)
+    return func
+
+def gen_iso_like_padding(begin_padder, repeat_padder):
+
+    # ISO/IEC 9797-1（消息认证码MAC的计算标准）
+    # Eg: 数据 + 80 + 00 00 00 ...
+    def func(data):
+        return repeat_append_singlebyte_padding(data + begin_padder, repeat_padder)
+    return func
 
 
-# TODO: will Cipher padding for us? default padding with zero?
-def sm4mac_without_padding(key: bytes, data: bytes) -> bytes:
+def pkcs7_padding(data: bytes) -> bytes:
+    padder = padding.PKCS7(128).padder()
+    return padder.update(data) + padder.finalize()
+
+def dummy_padding(data: bytes) -> bytes:
+    return data
+
+
+def sm4mac(key: bytes, data: bytes, iv: bytes = b'\x00' * 16, padding_methoh = dummy_padding):
     if len(key) != 16:
         raise ValueError("SM4 key must be 16 bytes")
 
-    iv = b'\x00' * 16
+    padded_data = padding_methoh(data)
+    print(f"Padded Data: {padded_data.hex()}")
+
     cipher = Cipher(algorithms.SM4(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
 
-    encrypted_data = encryptor.update(data) + encryptor.finalize()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
     return encrypted_data[-16:]
 
 def main() -> None:
@@ -26,10 +62,33 @@ def main() -> None:
 
     key = bytes.fromhex('11' * 16)  # 16-byte key
     data = bytes.fromhex('0000000000000000111111111111111122222222222222223333333333333333')
-
+    print("============================")
     print(f"Key: {key.hex()}")
     print(f"Data: {data.hex()}")
-    print(f"SM4 MAC: {sm4mac_without_padding(key, data).hex()}")
+
+    print("==============")
+    print(f"SM4 MAC: {sm4mac(key, data).hex()}")
+    print("==============")
+    print(f"SM4 PKCS MAC: {sm4mac(key, data, padding_methoh=pkcs7_padding).hex()}")
+    print("==============")
+    print(f"SM4 ISO MAC: {sm4mac(key, data, padding_methoh=pkcs7_padding).hex()}")
+
+    for i in [
+        0x00,
+        0x10, # same as PKCS7
+    ]:
+        padder = int.to_bytes(i)
+        print("==============")
+        print(f"SM4 Padder {padder.hex()} MAC: {sm4mac(key, data, padding_methoh=gen_pkcs7_like_padding(padder)).hex()}")
+
+    for begin_padder, repeat_padder in [
+        (0x80, 0x00) # ISO/IEC 9797-1 padding
+    ]:
+        begin_padder = int.to_bytes(begin_padder)
+        repeat_padder = int.to_bytes(repeat_padder)
+        print("==============")
+        print(f"SM4 ISO Padder MAC: {sm4mac(key, data, padding_methoh=gen_iso_like_padding(begin_padder, repeat_padder)).hex()}")
+
 
 
 if __name__ == "__main__":
