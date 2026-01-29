44cea46fc7a13e0ef9c8a39447e7f75c13f8e9e2
PEMessage <1165739182@qq.com>
2026-01-30 00:54:24 +0800

    embedded/mps2an385-cmake: add hello_world_fstackprotector, add working example

--- embedded/mps2an385-cmake/src/hello_world_fstackprotector/main.c
+++ embedded/mps2an385-cmake/src/hello_world_fstackprotector/main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 extern int stdout_init (void);
+
+extern uintptr_t __stack_chk_guard;
+void vulnerable_function() {
+    char buffer[10];
+
+    printf("Buffer address: %p\n", buffer);
+    printf("guard value is: %u \n", __stack_chk_guard);
+
+    strcpy(buffer, "THIS----THIS\n");
+
+    printf("%s", buffer);
+}
+
 
 int main() {
     stdout_init();
-    while(1){
-        // ZJW_C stdout_putchar('a');
-        printf("Hello world\n");
-    }
+    printf("Testing fstack-protector...\n");
+    char new[] = "This is something create on main";
+
+    vulnerable_function();
+    printf("If you see this, stack protection may be disabled\n");
+    printf("%s\n", new);
+
+    return 0;
+}
+
+// See: https://stackoverflow.com/a/42174846/20689546
+void __wrap___stack_chk_fail(void);
+void __real___stack_chk_fail(void);
+
+void __wrap___stack_chk_fail(void)
+{
+    printf("==== Hit ====\n");
+    __real___stack_chk_fail();
 }
