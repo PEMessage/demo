069101db3ebcb6de614eabbe7181fae15ae7b270
PEMessage <1165739182@qq.com>
2026-02-03 00:38:44 +0800

    rust/hello-world: add basic lambda demo

--- rust/hello-world/src/main.rs
+++ rust/hello-world/src/main.rs
@@ -11,27 +11,38 @@ struct Point {
 
 fn main() {
     // Part 1, hello world
-    println!("Hello, world!");
+    {
+        println!("Hello, world!");
+    }
 
 
     // Part 2, basic input
-    print!("Please input something: ");
-    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout"); // we have to flash
-                                                                          // before input
-                                                                          // Rust 的标准输出（stdout）默认是 行缓冲 的（在终端里）
-                                                                          // 即遇到换行符（\n）才会真正输出
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    {
+        print!("Please input something: ");
+        io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout"); // we have to flash
+                                                                              // before input
+                                                                              // Rust 的标准输出（stdout）默认是 行缓冲 的（在终端里）
+                                                                              // 即遇到换行符（\n）才会真正输出
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
 
-    println!("You entered: {}", input);
+        println!("You entered: {}", input);
+    }
 
     // Part 3, learn basic trait Debug / Default
-    let point1  = Point::default();
-    let point2 = Point { x: 1,  ..Point::default() };
+    {
+        let point1  = Point::default();
+        let point2 = Point { x: 1,  ..Point::default() };
 
-    dbg!(point1);
-    dbg!(point2);
+        dbg!(point1);
+        dbg!(point2);
+    }
+
+    // Part 4, lambda syntax
+    let _ = (|| {
+        println!("Done")
+    })();
 }
