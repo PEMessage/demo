dc523f645ebb37108e6d351e5a9fda2e568d3970
PEMessage <1165739182@qq.com>
2026-02-26 00:42:03 +0800

    embedded/zephyr: using justfile to replace

--- embedded/zephyr/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-WEST='uvx --with jsonschema --with pyelftools west'
-
-
-$WEST build -b mps2/an385 "$1"
-qemu-system-arm -machine mps2-an385 -kernel build/zephyr/zephyr.bin -serial stdio -d cpu_reset
