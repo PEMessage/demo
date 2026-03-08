510fa105a29732ca02ddaffbf40e3e9d3f9a43d9
PEMessage <1165739182@qq.com>
2026-03-09 00:46:21 +0800

    server/ansible/ping: init

---
+++ server/ansible/ping/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+# --inventory/-i: specify inventory host file or host list
+# --module-name/-m: specify action to execute
+ansible -i inventory.ini -m ping server
+ansible -i inventory.ini -m ping local
+ansible -i inventory.ini -m ping all
+
+
