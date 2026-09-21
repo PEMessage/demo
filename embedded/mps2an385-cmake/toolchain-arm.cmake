e2f51e89a320d88ab439947efa33642394809848
PEMessage <1165739182@qq.com>
2026-09-22 00:52:56 +0800

    embedded/mps2an385-cmake: remove clangd -isystem workaround

--- embedded/mps2an385-cmake/toolchain-arm.cmake
+++ embedded/mps2an385-cmake/toolchain-arm.cmake
@@ -11,8 +11,14 @@ set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
 set(CMAKE_OBJDUMP arm-none-eabi-objdump)
 set(CMAKE_SIZE arm-none-eabi-size)
 
-# Workaround to make clangd happy
-set(GCC_INCLUDE_FLAGS "-isystem /usr/lib/arm-none-eabi/include/")
+# # Workaround to make clangd happy
+# set(GCC_INCLUDE_FLAGS "-isystem /usr/lib/arm-none-eabi/include/")
+
+# Update 20260922:
+#   Thanks: https://discourse.nixos.org/t/get-clangd-to-find-standard-headers-in-nix-shell/11268
+if(CMAKE_EXPORT_COMPILE_COMMANDS)
+  set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
+endif()
 
 # Compiler flags
 set(CMAKE_C_FLAGS "-mcpu=cortex-m3 -mthumb -Wall -Os -g -g3 ${GCC_INCLUDE_FLAGS} -ffunction-sections -fdata-sections" CACHE STRING "C Compiler Flags")
